78b1d903d84648ac7b7ecb986007f142af3a1018
Wensses A. L <wensses_al2001@hotmail.com>
2021-09-26 20:44:55 -0500

    Proyecto Inicial

---
+++ P-Automotriz/P-Automotriz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P_Automotriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
